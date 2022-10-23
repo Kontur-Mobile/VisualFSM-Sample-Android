dc39e3a88bcbf02658237cb2042bbac836c78f62
g0rd1 <gordigev@gmail.com>
2022-10-23 19:11:40 +0500

    Add Kaspresso and create BaseTestCase

---
+++ app/src/androidTest/java/ru/kontur/mobile/visualfsm/sample_android/tests/BaseTestCase.kt
@@ -0,0 +1,30 @@
+package ru.kontur.mobile.visualfsm.sample_android.tests
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kaspersky.components.composesupport.config.withComposeSupport
+import com.kaspersky.components.composesupport.interceptors.behavior.impl.systemsafety.SystemDialogSafetySemanticsBehaviorInterceptor
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.params.FlakySafetyParams
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.runner.RunWith
+import ru.kontur.mobile.visualfsm.sample_android.MainActivity
+
+@RunWith(AndroidJUnit4::class)
+abstract class BaseTestCase : TestCase(
+    kaspressoBuilder = Kaspresso.Builder.withComposeSupport(
+        customize = {
+            flakySafetyParams = FlakySafetyParams.custom(timeoutMs = 5000, intervalMs = 1000)
+        },
+        lateComposeCustomize = { composeBuilder ->
+            composeBuilder.semanticsBehaviorInterceptors = composeBuilder.semanticsBehaviorInterceptors.filter {
+                it !is SystemDialogSafetySemanticsBehaviorInterceptor
+            }.toMutableList()
+        }
+    )
+) {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+}
