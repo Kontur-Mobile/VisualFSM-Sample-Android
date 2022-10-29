bcf7a6c1324f429a72b25fba78bfde793a06af3a
g0rd1 <gordigev@gmail.com>
2022-10-30 00:13:20 +0500

    reconfigure BaseTest case

--- app/src/androidTest/java/ru/kontur/mobile/visualfsm/sample_android/tests/BaseTestCase.kt
+++ app/src/androidTest/java/ru/kontur/mobile/visualfsm/sample_android/tests/BaseTestCase.kt
@@ -3,28 +3,22 @@ package ru.kontur.mobile.visualfsm.sample_android.tests
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kaspersky.components.composesupport.config.withComposeSupport
-import com.kaspersky.components.composesupport.interceptors.behavior.impl.systemsafety.SystemDialogSafetySemanticsBehaviorInterceptor
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
-import com.kaspersky.kaspresso.params.FlakySafetyParams
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.github.kakaocup.compose.node.element.ComposeScreen
 import org.junit.Rule
 import org.junit.runner.RunWith
 import ru.kontur.mobile.visualfsm.sample_android.MainActivity
 
 @RunWith(AndroidJUnit4::class)
 abstract class BaseTestCase : TestCase(
-    kaspressoBuilder = Kaspresso.Builder.withComposeSupport(
-        customize = {
-            flakySafetyParams = FlakySafetyParams.custom(timeoutMs = 5000, intervalMs = 1000)
-        },
-        lateComposeCustomize = { composeBuilder ->
-            composeBuilder.semanticsBehaviorInterceptors = composeBuilder.semanticsBehaviorInterceptors.filter {
-                it !is SystemDialogSafetySemanticsBehaviorInterceptor
-            }.toMutableList()
-        }
-    )
+    kaspressoBuilder = Kaspresso.Builder.withComposeSupport()
 ) {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    inline fun <reified T : ComposeScreen<T>> onComposeScreen(noinline function: T.() -> Unit): T {
+        return ComposeScreen.onComposeScreen(composeTestRule, function)
+    }
 }
